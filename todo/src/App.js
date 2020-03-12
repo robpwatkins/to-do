97c46b582e05ff315a66ab429e059c84ddaa6af7
Rob W <robpwatkins@gmail.com>
2020-03-11 23:09:33 -0500

    yes

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,13 +1,35 @@
 import React from 'react';
 import './App.css';
 
+
+
 class App extends React.Component {
-  
+  state = {
+    input: '',
+    text: []
+  }
+
+  updateInput = event => {
+    this.setState({ input: event.target.value })
+  }
+
+  formSubmit = event => {
+    event.preventDefault()
+    this.setState({
+      text: [ ...this.state.text, this.state.input],
+      input: ''
+    })
+    console.log(this.state.text)
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <h1>heyoo</h1>
+          <form onSubmit={this.formSubmit}>
+            <input value={this.state.input} onChange={this.updateInput}></input>
+            <button>Submit</button>
+          </form>
         </header>
       </div>
     );
