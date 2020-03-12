2a9c313eba357dfac8381d523885b947501908db
Rob W <robpwatkins@gmail.com>
2020-03-12 17:24:47 -0500

    yes

--- todo/src/App.js
+++ todo/src/App.js
@@ -6,7 +6,8 @@ import './App.css';
 class App extends React.Component {
   state = {
     input: '',
-    todos: []
+    todos: [],
+    // count: 0
   }
 
   updateInput = event => {
@@ -25,10 +26,27 @@ class App extends React.Component {
     this.setState({ todos: this.state.todos.filter((x, y) => y !== index) })
   }
 
+  // increment = () => {
+  //   this.setState({ 
+  //     count: this.state.count + 1 
+  //   })
+  // }
+  
+  // decrement = () => {
+  //   this.setState({ 
+  //     count: this.state.count - 1 
+  //   })
+  // }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
+          {/* <div>
+            <div>count: {this.state.count}</div>
+            <button onClick={this.increment}>increment</button>
+            <button onClick={this.decrement}>decrement</button>
+          </div> */}
           <form onSubmit={this.formSubmit}>
             <input value={this.state.input} onChange={this.updateInput}></input>
             <button>Submit</button>
