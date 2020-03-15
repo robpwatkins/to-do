4b169e3356987a35d25754b28bcd1cb93dd479df
Rob W <robpwatkins@gmail.com>
2020-03-15 17:22:38 -0500

    added a tiny bit of styling

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './App.css';
-// import TodoItem from './TodoItem';git
 
 
 
@@ -8,7 +7,6 @@ class App extends React.Component {
   state = {
     input: '',
     todos: [],
-    // count: 0
   }
 
   updateInput = event => {
@@ -27,40 +25,22 @@ class App extends React.Component {
     this.setState({ todos: this.state.todos.filter((x, y) => y !== index) })
   }
 
-  // increment = () => {
-  //   this.setState({ 
-  //     count: this.state.count + 1 
-  //   })
-  // }
-  
-  // decrement = () => {
-  //   this.setState({ 
-  //     count: this.state.count - 1 
-  //   })
-  // }
-
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          {/* <div>
-            <div>count: {this.state.count}</div>
-            <button onClick={this.increment}>increment</button>
-            <button onClick={this.decrement}>decrement</button>
-          </div> */}
-          <form onSubmit={this.formSubmit}>
-            <input value={this.state.input} onChange={this.updateInput}></input>
-            <button>Submit</button>
-          </form>
-          {this.state.todos.map((item, index) => {
+        <h2>Todos:</h2>
+        <form onSubmit={this.formSubmit}>
+          <input value={this.state.input} onChange={this.updateInput}></input>
+          <button>Submit</button>
+        </form>
+        {this.state.todos.map((item, index) => {
           return (
-            <div key={index}>
+            <div className="todo" key={index}>
               {this.state.todos[index]}
-              <button onClick={() => this.deleteElement(index)}>delete</button>
+              <button onClick={() => this.deleteElement(index)}>done</button>
             </div>
-          )
+            )
         })}
-        </header>
       </div>
     );
   }
